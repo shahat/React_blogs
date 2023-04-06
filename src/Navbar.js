5f08f630317dddc95e00d1ab3014099f0a2ed939
shahat <me6847570@gmail.com>
2023-04-06 05:50:43 +0200

    this is the react learing v_19 in react ninja

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <a href="/">Home</a>
+        <a class="createBlog" href="/create">
+          New Blog
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
