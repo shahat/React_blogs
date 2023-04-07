572a26fccf764827e69e4ba8b02bb3dc5b6f1f17
shahat <me6847570@gmail.com>
2023-04-07 07:47:06 +0200

    this is the end of the custom hooks

--- src/Home.js
+++ src/Home.js
@@ -1,61 +1,17 @@
-import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
-
+import useFetch from "./useFetch";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  /* 
- handle delete was orking with older version of this project so we now we will do delete event using api 
- 
- */
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id); // return array of object
-  //   setBlogs(newBlogs);
-  // };
-
-  /* optaining the data from json server using fetch method  */
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok)
-            throw Error(
-              "Error cant fetch the data fetch the data for that resource "
-            );
-
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setIsPending(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000); // end of setTimeOut we used setTimeOut to make fetching the data more realistic
-  }, []); // U should make the empty array for not going into infinry loop
-
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
   return (
     <div className="home">
-      {/* showing the errror  */}
       {error && <div> {error} </div>}
-      {/* 
-      Note that  => we use the conditional rendering 
-      bcz data take some time to load 
-
-      blogs not be render untill the blogs at the beginning have a value not null
-      */}
       {isPending && <div> Loading ... </div>}
-
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
-
-      {/* <button onClick={handelDelete}> click here </button> */}
     </div>
   );
 };
-
 export default Home;
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,2 +1,37 @@
+import { useState, useEffect } from "react";
 // custom hooks should start with the word use
-const useFetch = () => {};
+// this hook now is more reusable to use
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    // end of setTimeOut we used setTimeOut to make fetching the data more realistic
+    setTimeout(() => {
+      fetch(url) // we import the url here to make it more reusable for any url
+        .then((res) => {
+          if (!res.ok)
+            throw Error(
+              "Error cant fetch the data fetch the data for that resource"
+            );
+          return res.json();
+        })
+        .then((data) => {
+          setData(data);
+          setIsPending(false);
+          setIsPending(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 1000);
+  }, [url]);
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+export default useFetch;
